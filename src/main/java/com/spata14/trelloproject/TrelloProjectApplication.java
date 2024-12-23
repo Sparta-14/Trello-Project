726b859444dc6f5acc3307574a61ee60bee4ccd7
Park JaeHong <blogerhong@naver.com>
2024-12-23 15:57:13 +0900

    chore : 초기 세팅, build.gradle, application.yml 세팅
    - BaseEntity, PasswordEncoder 추가

---
+++ src/main/java/com/spata14/trelloproject/TrelloProjectApplication.java
@@ -0,0 +1,15 @@
+package com.spata14.trelloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TrelloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloProjectApplication.class, args);
+    }
+
+}
