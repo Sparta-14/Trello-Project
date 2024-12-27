002c05d4ed5adaab745814e23b96e1aeeb3944cf
gyoungsubLee <dltjq2323@gmail.com>
2024-12-27 21:47:45 +0900

    feat: DTO UpdateNotificationRequestDto

---
+++ src/main/java/com/spata14/trelloproject/Notification/dto/UpdateNotificationRequestDto.java
@@ -0,0 +1,11 @@
+package com.spata14.trelloproject.Notification.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateNotificationRequestDto {
+
+    private Boolean isRead;
+}
