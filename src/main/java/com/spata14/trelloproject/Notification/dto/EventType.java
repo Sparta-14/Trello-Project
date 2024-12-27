71ada21d0ac987727f69f6a4a892bcb1fe794fe1
gyoungsubLee <dltjq2323@gmail.com>
2024-12-27 20:16:14 +0900

    feat: Enum EventType

---
+++ src/main/java/com/spata14/trelloproject/Notification/dto/EventType.java
@@ -0,0 +1,48 @@
+package com.spata14.trelloproject.Notification.dto;
+
+/**
+ * Enumeration representing various event types in the system.
+ * <p>
+ * Each event type is associated with a string identifier.
+ * </p>
+ */
+public enum EventType {
+
+    /**
+     * Represents an event related to a workspace.
+     */
+    WORKSPACE("workspace"),
+
+    /**
+     * Represents an event related to a card.
+     */
+    CARD("card"),
+
+    /**
+     * Represents an event related to a comment.
+     */
+    COMMENT("comment");
+
+    /**
+     * The string identifier associated with the event type.
+     */
+    private String type;
+
+    /**
+     * Constructs an {@link EventType} with the specified string identifier.
+     *
+     * @param type the string identifier for the event type
+     */
+    EventType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Returns the string identifier of the event type.
+     *
+     * @return the string identifier of the event type
+     */
+    public String getType() {
+        return type;
+    }
+}
