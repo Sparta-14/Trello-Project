65084fb5fe775dc35fc92e0cbe1902c8b256c5d5
gyoungsubLee <dltjq2323@gmail.com>
2024-12-27 21:48:22 +0900

    feat: Repository NotificationRepository

---
+++ src/main/java/com/spata14/trelloproject/Notification/repository/NotificationRepository.java
@@ -0,0 +1,22 @@
+package com.spata14.trelloproject.Notification.repository;
+
+import com.spata14.trelloproject.Notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, String> {
+
+    default Notification findByIdOrElseThrow(String id) {
+        return findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 알림입니다. id = " + id)
+        );
+    }
+
+    // SELECT * FROM notification WHERE is_read = false ODERBY created_at DESC;
+    List<Notification> findAllByIsReadFalseOrderByCreatedAtDesc();
+}
