1d6e1ee110587047383c1bb4743a5261efd2cc8f
gyoungsubLee <dltjq2323@gmail.com>
2024-12-28 15:30:34 +0900

    refact: API 호출 로직 제거 • 조회, 업데이트, 삭제, Dto

--- src/main/java/com/spata14/trelloproject/Notification/repository/NotificationRepository.java
+++ src/main/java/com/spata14/trelloproject/Notification/repository/NotificationRepository.java
@@ -10,10 +10,4 @@ import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, String> {
-
-    default Notification findByIdOrElseThrow(String id) {
-        return findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 알림입니다. id = " + id)
-        );
-    }
 }
